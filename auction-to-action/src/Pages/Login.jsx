29ccf13693e7b11f75b38cbb66d2b231578065e7
Narayanan <csnarayanan2006@gmail.com>
2025-08-09 19:27:36 +0530

    feat(auth): add placeholder for user login page
    - Creates a blank placeholder component for the user login page.
    - Establishes the file structure for the user authentication flow.

--- auction-to-action/src/Pages/Login.jsx
+++ auction-to-action/src/Pages/Login.jsx
@@ -1,23 +1,46 @@
-import { Box, Button, Divider } from '@chakra-ui/react'
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Box, Button, VStack, Heading, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const navigate = useNavigate();
 
   return (
-    <Box className="login-container">
-      <Button colorScheme='blue' onClick={() => navigate('/admindashboard')}>
-        Admin Login
-      </Button>
-      <br /><br />
-      <Divider orientation='horizontal' />
-      <br /><br />
-      <Button colorScheme='green' onClick={() => navigate('/userdashboard')}>
-        User Login
-      </Button>
+    <Box
+      minH="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      bgGradient="linear(to-br, #6BA3BE, #0C969C)"
+    >
+      <VStack spacing={8}>
+        <Box textAlign="center">
+          <Heading color="white" size="2xl">
+            Auction to Action
+          </Heading>
+          <Text color="gray.200" fontSize="lg" mt={2}>
+            Please select your login type
+          </Text>
+        </Box>
+        <VStack spacing={4} align="stretch" w="xs">
+          <Button
+            colorScheme="purple"
+            size="lg"
+            onClick={() => navigate("/admin-login")}
+          >
+            Admin Login
+          </Button>
+          <Button
+            colorScheme="teal"
+            size="lg"
+            onClick={() => navigate("/user-login")}
+          >
+            User Login
+          </Button>
+        </VStack>
+      </VStack>
     </Box>
-  )
+  );
 }
 
-export default Login
+export default Login;
