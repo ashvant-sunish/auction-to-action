4a8a51b834efaa76303e6269ea651ae6cf105bed
Narayanan <csnarayanan2006@gmail.com>
2025-08-09 19:25:37 +0530

    refactor(routing): integrate new login routes
    - Updates the main router in App.jsx to include the new routes for the login selection page, admin login, and user login.
    - Ensures the application correctly navigates between the different authentication views.

--- auction-to-action/src/App.jsx
+++ auction-to-action/src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import Login from './Pages/Login';
-import DashboardAdmin from './Pages/Admin/Dashboard.Admin';
-import DashboardUser from './Pages/User/Dashboard.User';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import DashboardAdmin from "./Pages/Admin/Dashboard.Admin";
+import DashboardUser from "./Pages/User/Dashboard.User";
+import AdminLogin from "./Pages/Admin/AdminLogin";
+import UserLogin from "./Pages/User/UserLogin";
 
 function App() {
-  const navigate = useNavigate()
-
   return (
     <Routes>
       <Route exact path="/" element={<Login />} />
+      <Route path="/admin-login" element={<AdminLogin />} />
+      <Route path="/user-login" element={<UserLogin />} />
       <Route path="/admindashboard" element={<DashboardAdmin />} />
       <Route path="/userdashboard" element={<DashboardUser />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
