0e80cf4a37fb8e4f188b9c64874ee43bf30fd442
Narayanan <csnarayanan2006@gmail.com>
2025-08-27 00:06:10 +0530

    feat(theme): implement Chakra UI custom theme

--- auction-to-action/src/Theme/theme.js
+++ auction-to-action/src/Theme/theme.js
@@ -1,24 +1,22 @@
-import { border, extendTheme } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
- const customTheme = {
-      colors: {
-        primary: {
-          50: '#6BA3BE',
-          100:  '#0C969C',
-          150: '#0A7075',
-          200: '#032F30',
-        },
-        dark: '#031716',
-        bg: '#274D60'
-        // Add or override other colors
-      },
-      fonts: {
-        heading: ' Georgia, serif',  
-        body: ' Montserrat, sans-serif',
-      },
-      // ... more customizations
-    };
+const customTheme = {
+  colors: {
+    primary: {
+      50: '#6BA3BE',
+      100: '#0C969C',
+      150: '#0A7075',
+      200: '#032F30',
+    },
+    dark: '#031716',
+    bg: '#274D60'
+  },
+  fonts: {
+    heading: "'Inter', sans-serif",
+    body: "'Inter', sans-serif",
+  },
+};
 
-    const theme = extendTheme(customTheme);
+const theme = extendTheme(customTheme);
 
-    export default theme;
+export default theme;
