1484aaba33514a50eef537bf42a793f35325171c
Aditya Pratap Singh Jadon <adityapratap2904@gmail.com>
2026-09-11 05:20:14 +0530

    round 2: fixed

--- Backend/server.js
+++ Backend/server.js
@@ -130,7 +130,7 @@ io.on('connection', (socket) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
